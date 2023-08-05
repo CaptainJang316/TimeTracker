2b1774e78b0e16c250922fa8da5a20d2e1b82a47
Jang <gksrkfkaryghl@gmail.com>
2023-08-05 23:46:41 +0900

    add valiadtion for initial task input

--- src/Clock.tsx
+++ src/Clock.tsx
@@ -6,7 +6,6 @@ import styled from 'styled-components';
 const InitialTaskInput = styled.input`
   width: 400px;
   height: 50px;
-  margin: 30px;
   font-size: 1.5rem;
 `; 
 
@@ -14,6 +13,12 @@ const StartButton = styled.button`
   width: 400px;
   height: 50px;
   font-size: 1.8rem;
+  margin: 30px;
+`;
+
+const ValidationMessage = styled.div`
+  color: red;
+  text-align: start;
 `;
 
 
@@ -25,6 +30,7 @@ function Clock() {
   const [time, setTime] = useState(new Date());
   const [currentTaskTitle, setCurrentTaskTitle] = useState("");
   const [items, setItems] = useState<string[]>([]);
+  const [showvalidationError, setShowvalidationError] = useState(false);
   const [itemTitle, setItemTitle] = useState<string>("");
 
 
@@ -50,7 +56,10 @@ function Clock() {
   }
 
   const handleStartButtonClick = () => {
-    setStartFlag(true);
+    if(currentTaskTitle.trim() != "") {
+      setStartFlag(true);
+      setShowvalidationError(false);
+    } else setShowvalidationError(true);
   }
 
   return (
@@ -63,6 +72,7 @@ function Clock() {
       <List items={items}/>
   </> : <>
     <InitialTaskInput type="text" value={currentTaskTitle} onChange={handleInputChange} placeholder="What to do now"/>
+    <ValidationMessage>{showvalidationError? "Please enter your first task." : ""}</ValidationMessage> 
     <StartButton onClick={handleStartButtonClick}>Start Time Tracking</StartButton> 
   </>
     
